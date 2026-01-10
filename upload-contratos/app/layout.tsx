5d52543aa3950df89eddc56c1aa942332b71c7de
anthony-reis <anthonsareis11@gmail.com>
2026-01-10 11:21:37 -0300

    feat: adiciona os providers na aplicação

--- upload-contratos/app/layout.tsx
+++ upload-contratos/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Cade Consig",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className="h-full">
-      <body className="h-full">{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
